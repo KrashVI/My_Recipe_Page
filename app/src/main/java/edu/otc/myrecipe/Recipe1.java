0bdc4ea3393bcf1ecddbf67c43acd90b95261231
Sean <springs@otc.edu>
2024-02-05 10:31:02 -0600

    Initial commit

---
+++ app/src/main/java/edu/otc/myrecipe/Recipe1.java
@@ -0,0 +1,14 @@
+package edu.otc.myrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recipe1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe1);
+    }
+}
